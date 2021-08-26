f46a3b9f99b5049e25ec4917fc77f8fa2a8afe8b
A <pixaxing@outlook.com>
2021-08-26 11:51:08 -0600

    Progress With Survey Response Totals

---
+++ interview-a/client/src/components/AnswerTotal.tsx
@@ -0,0 +1,21 @@
+﻿import React from "react";
+
+interface AnswerTotalProps {
+    answer: string;
+    count: number;
+}
+
+const AnswerTotal = ({ answer, count }: AnswerTotalProps) => {
+    return (
+        <div>
+            <div>
+                <h2>{answer}</h2>
+            </div>
+            <div>
+                {count}
+            </div>
+        </div>
+    );
+}
+
+export default AnswerTotal;
---
+++ interview-a/client/src/views/ResponseTotals.tsx
@@ -0,0 +1,45 @@
+﻿import { useState, useEffect } from "react";
+import SurveyResponse, { ISurveyResponse } from "../entities/SurveyResponse";
+import AnswerTotal from "../components/AnswerTotal";
+
+const ResponseTotals = () => {
+    const [surveyResponses, setSurveyResponses] = useState<ISurveyResponse[] | null>();
+
+    useEffect(() => {
+        const loadResponses = async (): Promise<void> => {
+            const response = await fetch(`http://localhost:2047/api/responses/all`);
+            let data;
+            try {
+                data = await response.json();
+            } catch (error) {
+                console.error(error);
+                data = null;
+            }
+
+            if (response.ok) {
+                setSurveyResponses(data.responses)
+            } else {
+                console.error(`API failure: ${response.status}`, data);
+            }
+        }
+
+        loadResponses();
+    }, []);
+
+
+    if (surveyResponses) {
+    }
+
+    return (
+        <>
+            {surveyResponses!.map(sResponse => (
+                <div>
+                    <h1>{sResponse.content.questions[0].question}</h1>
+                    <AnswerTotal answer={sResponse.content.questions[0].answer} count={1} />
+                </div>
+            ))}
+        </>
+    );
+}
+
+export default ResponseTotals;
